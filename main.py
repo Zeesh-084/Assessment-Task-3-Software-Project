e0acaaa74cd7e3f09d03791a5a84f8f5a5e57a85
Zeesh-084 <zeeshali2409@gmail.com>
2026-07-10 18:43:19 +1000

    minor adjustments to the login page

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ from flask import redirect
 import model as dbHandler
 
 app = Flask(__name__)
+
+@app.route('/login.html', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if dbHandler.validate_user(username, password):
+            return redirect('/dashboard.html')
+        else:
+            return render_template('/login.html', error='Invalid username or password')
+        return render_template('/login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
