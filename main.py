c088f1cac297d44f657312418fecc5b036622f57
Zeesh-084 <zeeshali2409@gmail.com>
2026-07-10 20:52:43 +1000

    Main.py works

--- main.py
+++ main.py
@@ -6,18 +6,10 @@ import model as dbHandler
 
 app = Flask(__name__)
 
-@app.route('/login.html', methods=['GET'])
+@app.route('login.html', methods=['GET', 'POST'])
 @app.route('/', methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if dbHandler.validate_user(username, password):
-            return redirect('/dashboard.html')
-        else:
-            return render_template('/login.html', error='Invalid username or password')
-        return render_template('/login.html')
-
+  return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0', port=5000)
