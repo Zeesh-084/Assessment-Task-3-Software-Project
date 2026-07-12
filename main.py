02904abbf41057f4c52099e1246ae1c6449c488b
Zeesh-084 <zeeshali2409@gmail.com>
2026-07-13 01:30:44 +1000

    adjustments

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 @app.route('/login.html', methods=['GET', 'POST'])
 @app.route('/', methods=['POST', 'GET'])
 def login():
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+    if dbHandler.validate_user(username, password):
+      return redirect('/index.html')
+    else:
+      return render_template('login.html', error='Invalid username or password')
   return render_template('login.html')
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,1 +1,16 @@
 import sqlite3 as sql
+
+def insertUser(username, password):
+    conn = sql.connect('../database.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    conn.commit()
+    conn.close()
+
+def retrieveUsers():
+    conn = sql.connect('../database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT username, password FROM users")
+    users = cursor.fetchall()
+    conn.close()
+    return users
