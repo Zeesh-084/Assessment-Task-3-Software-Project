ead45d9f1f0135aafdfc4ba94c6e7a3c50cd2e67
Zeesh-084 <zeeshali2409@gmail.com>
2026-07-18 01:03:15 +1000

    fixed login and signup page alignment issue almost

--- model.py
+++ model.py
@@ -1,16 +1,16 @@
 import sqlite3 as sql
 
-def insertUser(username, password):
+def insertUser(username, password, email):
     conn = sql.connect('../database.db')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    cursor.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", (username, password, email))
     conn.commit()
     conn.close()
 
 def retrieveUsers():
     conn = sql.connect('../database.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT username, password FROM users")
+    cursor.execute("SELECT username, password, email FROM users")
     users = cursor.fetchall()
     conn.close()
     return users
